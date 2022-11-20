0ec2477dd806940b1f61aaf61ebbfc0963d4ba51
LwinKoK <lwinkoko0271@gmail.com>
2022-11-20 09:47:07 +0630

    modal box

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="showModal">
+    <BackModal
+      :title="title"
+      :content="content"
+      :theme="theme"
+      @close="showModal = false"
+    />
+  </div>
+  <div v-if="showModalTwo">
+    <BackModal
+      title="Are u sure want to delete?"
+      content="Yes or No"
+      theme="delete"
+      @close="showModalTwo = false"
+    />
+  </div>
+  <button @click="showModal = true">Show Login Modal</button>
+  <button @click="showModalTwo = true" class="orange">Show Delete Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BackModal from "./components/BackModal.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      title: "Login Success",
+      content: "Welcome user Lwin Ko Ko",
+      theme: "success",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    BackModal,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+button {
+  padding: 15px 20px;
+  border: none;
+  background-color: royalblue;
+  color: whitesmoke;
+  margin: 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+}
+.orange {
+  background-color: orangered;
+  margin-left: 5px;
 }
 </style>
---
+++ src/components/BackModal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="backdrop" @click.self="hideModal">
+    <div
+      class="modal"
+      :class="{ success: theme === 'success', danger: theme === 'delete' }"
+    >
+      <h2>{{ title }}</h2>
+      <p>{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "content", "theme"],
+  methods: {
+    hideModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.backdrop {
+  top: 0;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.modal {
+  width: 500px;
+  background-color: white;
+  margin: 100px auto;
+  border-radius: 10px;
+  padding: 25px;
+}
+h2 {
+  margin-bottom: 10px;
+}
+.success {
+  background-color: green;
+  color: whitesmoke;
+}
+.danger {
+  background-color: crimson;
+  color: white;
+}
+</style>
